07e92c2ff9068dc70550371875c11c918a3b81a9
Enr1que3 <99999296+Enr1que3@users.noreply.github.com>
2022-09-20 17:10:59 -0500

    numeros del 100 al 1

---
+++ 04-Control-de-flujo/condicionales.py
@@ -0,0 +1,15 @@
+# > mayor
+# >= mayor o igual
+# == exactamene igual
+# <= menor o igual
+# < menor que
+
+
+#Escribe un programa que sea capaz de mostrar los numero del 1 al 100 en ordern inverso
+
+
+cien=100
+while cien>0:
+    print(cien)
+    cien-=1
+
