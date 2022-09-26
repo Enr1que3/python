35e695e60e599d9948dc6d4b2f150e894162bad8
Enr1que3 <99999296+Enr1que3@users.noreply.github.com>
2022-09-26 14:32:31 -0500

    clases con python

---
+++ clases.py
@@ -0,0 +1,16 @@
+class Dino:
+    #propiedad
+    encendido=True
+
+    #metodos(Funcones)
+    def  apaga():
+        self.encendido=False
+
+#instanciar una clase significa crear un objeto
+
+#crear objeto
+d=Dino()
+d.enceiende=False #se puede sobreescrir en cualquier momentos
+print(d.enceiende)
+
+
