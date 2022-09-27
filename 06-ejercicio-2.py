3de1bee120e33b6efca1ef2e0ca892509b44efaa
Enr1que3 <99999296+Enr1que3@users.noreply.github.com>
2022-09-27 11:01:24 -0500

    tema 6, ejercicio 2

---
+++ 06-ejercicio-2.py
@@ -0,0 +1,19 @@
+class Alumno:
+    nombre=None
+    nota=None
+    def __init__(self,nombre,nota):
+        self.nombre=nombre
+        self.nota=nota
+        if nota>=60:
+            print(nombre,"Aprobado con",nota)
+        else:
+            print(nombre,"Reprobado con",nota)
+    
+    def calificacion(nombre, nota):
+        pass
+        
+    
+
+a=Alumno("Enrique",50)
+
+
