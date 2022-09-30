1e10cafb3c0ebb7bc81fc1db1482bb91a17d924a
Enr1que3 <99999296+Enr1que3@users.noreply.github.com>
2022-09-30 21:36:45 +0200

    tema 7 ejercicio 2

---
+++ 07-ejercicio-2.py
@@ -0,0 +1,17 @@
+import time
+
+hora_actual=12
+#imprir la hora
+while True:
+    hora_actual=time.strftime("%H:%M:%S")
+    print(hora_actual)
+
+    
+    time.sleep(60)
+
+if hora_actual>=7:
+    print("Es hora de ir se a casa")
+else:
+    hora_actual=time.time()
+    hora_final=time.time()
+    tiempo_restante = hora_final-hora_actual
