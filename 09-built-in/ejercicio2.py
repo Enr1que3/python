386f129b9a151a09a47b550011de22b0e59eaa65
Enr1que3 <99999296+Enr1que3@users.noreply.github.com>
2022-10-17 21:34:18 +0200

    Update ejercicio2.py

--- 09-built-in/ejercicio2.py
+++ 09-built-in/ejercicio2.py
@@ -1,14 +1,13 @@
 '''En este segundo ejercicio, tenéis que crear una aplicación que obtendrá los elementos impares de una lista pasada por parámetro con filter y realizará una suma de todos estos elementos obtenidos mediante reduce.'''
 
 from functools import reduce
-numeros =[1,3,5,7,9,11,13]
 
-def impares(x):
-    impar=filter(numeros)
+numerosImpares=[1,2,3,4,5,6,7,8,9]
 
-res=impares(numeros)
+def sumaImpares(suma):
+    if suma % 2 !=0:
+        return True
 
-print(res)
+resultado=filter(sumaImpares,numerosImpares)
 
-    
-        
+print(list(resultado))
